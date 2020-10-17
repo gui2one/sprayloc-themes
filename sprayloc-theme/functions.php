2fca834239a5095bd6719d25544203bb8740c9b2
gui2one <guillaume.rouault.fx@gmail.com>
2020-10-17 22:06:34 +0200

    first commit

---
+++ sprayloc-theme/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package sprayloc
+ */
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_template_directory_uri(  ). '/style.css');
+}
