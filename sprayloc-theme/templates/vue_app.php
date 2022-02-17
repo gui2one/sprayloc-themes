9bc08205104c38dd630df49602dfe20b466b7299
gui2one <guillaume.rouault.fx@gmail.com>
2022-02-17 20:54:44 +0100

    see diffs

--- sprayloc-theme/templates/vue_app.php
+++ sprayloc-theme/templates/vue_app.php
@@ -27,26 +27,25 @@ if (user_can($current_user, 'administrator')) {
         </div>
     </div>
 
-    <div v-if="true">
 
-        <div v-if="paginated_items.length == 0" class="no-equipment">
-            Aucun équipement dans cette catégorie.
-            <br> <a href="?category=all">Voir tous les équipements</a>
-        </div>
-        <div class="cards-container" v-if="window_width > 500">
-            <sprayloc-card   v-for="item in paginated_items" v-bind:data="item"
-                v-bind:image="getImageThumbnail(item.image) ? getImageThumbnail(item.image) : getImageThumbnail(item.images[0])"
-                @show-details="showDetails" v-bind:folder="getFolderName(item.folder)" :key="item.id"></sprayloc-card>
-        </div>
-        <div class="cards-container" v-else>
-            <sprayloc-item-row v-for="item in getPaginatedItems(this.pagination_start,this.pagination_end)" v-bind:data="item"
-                v-bind:image="getImageThumbnail(item.image) ? getImageThumbnail(item.image) : getImageThumbnail(item.images[0])"
-                @show-details="showDetails" v-bind:folder="getFolderName(item.folder)" :key="item.id"></sprayloc-item-row>
-        </div>
-         <sprayloc-pagination :numcards="filtered.length" :filtered="filtered" :maxitems="pagination_max" @page-change="onPageChange" @change-pagination-max="onChangePaginationMax"></sprayloc-pagination>
-        <detail-vue v-if="data_loaded" :item="getEquipmentByID(id_selected)" :kits="kits" @hide-details="hideDetails"
-            @show-details="showDetails" />
+    <div v-if="paginated_items.length == 0" class="no-equipment">
+        Aucun équipement dans cette catégorie.
+        <br> <a href="?category=all">Voir tous les équipements</a>
     </div>
+    <div class="cards-container" v-if="window_width > 500">
+        <sprayloc-card   v-for="item in paginated_items" v-bind:data="item"
+            v-bind:image="getImageThumbnail(item.image) ? getImageThumbnail(item.image) : getImageThumbnail(item.images[0])"
+            @show-details="showDetails" v-bind:folder="getFolderName(item.folder)" :key="item.id"></sprayloc-card>
+    </div>
+    <div class="cards-container" v-else>
+        <sprayloc-item-row v-for="item in getPaginatedItems(this.pagination_start,this.pagination_end)" v-bind:data="item"
+            v-bind:image="getImageThumbnail(item.image) ? getImageThumbnail(item.image) : getImageThumbnail(item.images[0])"
+            @show-details="showDetails" v-bind:folder="getFolderName(item.folder)" :key="item.id"></sprayloc-item-row>
+    </div>
+        <sprayloc-pagination class="pagination-bottom" :numcards="filtered.length" :filtered="filtered" :maxitems="pagination_max" @page-change="onPageChange" @change-pagination-max="onChangePaginationMax"></sprayloc-pagination>
+    <detail-vue v-if="data_loaded" :item="getEquipmentByID(id_selected)" :kits="kits" @hide-details="hideDetails"
+        @show-details="showDetails" />
+
 
 </div>
 <script src="<?php echo get_stylesheet_directory_uri();?>/templates/vue_app.js" ></script>
